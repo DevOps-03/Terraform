7f974d7773dec00888034a4eb9648fe479e7a09d
DevOps-03 <girishr1518@gmail.com>
2025-09-24 19:48:18 +0530

    Update output.tf

--- env/main.tf/variables.tf/output.tf
+++ env/main.tf/variables.tf/output.tf
@@ -1,1 +1,15 @@
+# ----- Provider Configuration -----
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
+
+# ----- EC2 Instance -----
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (us-east-1)
+  instance_type = "t3.micro"               # Free-tier eligible
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
 
